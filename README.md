2c9862df521d83f539b147bbd0b709f6fbd7c295
awbtux <andy@antareslinux.org>
2024-01-31 10:55:44 -0800

    clean up main(), -n/--names flag, file functions

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## To-do
-- [ ] -n/--names: read a list of names from a file at runtime, or from stdin if '-' is passed
+- [ ] -n/--names: read a list of names from stdin if '-' is passed
 - [ ] when -P and -s are set, attempt to substitute the old hostname with the new one in a file, instead of overwriting the whole file
 - [ ] make an actual build system (other than `$ cc main.c -o hostgen`)
 - [ ] write readme
--- main.c
+++ main.c
@@ -13,10 +13,20 @@
 // preproc macros for version info for now, no build system yet
 #define HOSTGEN_VERSION "v0.3a"
 
-// char arrays
-char *program_name, *option_output_file, *option_prefix = "", *option_suffix = "";
+// file descriptors
+FILE *names_location,
+     *random_file,
+     *write_location;
 
-// affects fopen permissions in prepare_file()
+// char arrays
+char *program_name,
+     *option_names_file,
+     *option_output_file,
+     *option_platform = "",
+     *option_prefix = "",
+     *option_suffix = "";
+
+// affects fopen permissions in file_prepare()
 bool option_append_writes = false;
 
 // platform type enum
@@ -30,11 +40,12 @@ enum EPlatformType {
 // command line options
 static struct option long_options[] = {
     {"append", no_argument, 0, 'a'},
+    {"help", no_argument, 0, 'h'},
+    {"names", required_argument, 0, 'n'},
     {"output", required_argument, 0, 'o'},
     {"platform", required_argument, 0, 'p'},
     {"prefix", required_argument, 0, 'P'},
     {"suffix", required_argument, 0, 's'},
-    {"help", no_argument, 0, 'h'},
     {"version", no_argument, 0, 'v'},
     {0, 0, 0, 0}
 };
@@ -48,6 +59,7 @@ void display_help_message(void) {
            "  -a, --append              append to FILE, instead of overwriting or trying\n"
            "                            substitution in it\n"
            "  -h, --help                display this message\n"
+           "  -n, --names=NAMEFILE      choose a list of firstnames other than the defaults\n"
            "  -o, --output=FILE         write the new hostname to FILE instead of stdout\n"
            "  -p, --platform=PLATFORM   make the hostname emulate the default for PLATFORM\n"
            "  -P, --prefix=PREFIX       prepend PREFIX to the hostname string\n"
@@ -75,6 +87,98 @@ void display_version_message(void) {
     exit(0);
 }
 
+
+// count the lines in a file
+int file_count_lines(FILE *file) {
+    // char and line
+    int ch = 0, ln = 0;
+
+    // if an invalid file descriptor is passed, return a negative line count
+    if (!file)
+        return -1;
+
+    // rewind the file to the beginning
+    rewind(file);
+
+    // increment the line count
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == '\n') {
+            ln++;
+        }
+    }
+
+    // return the line number
+    return ln;
+}
+
+// get a random line from a file
+char *file_get_random_line(FILE *file) {
+    // buffer holding the chosen line
+    char line_bfr[1024], *line = line_bfr;
+
+    // get the total number of lines in the file
+    int lines = file_count_lines(file);
+
+    // return null if lines can't be counted
+    if (lines <= 0)
+        return NULL;
+
+    // generate a random line number
+    int random_line = (rand() % lines) + 1;
+
+    // seek to the beginning of the file
+    rewind(file);
+
+    // read and discard lines until the randomly chosen line
+    for (int i = 1; i < random_line; i++) {
+        char bfr[1024];
+        if (fgets(bfr, sizeof(bfr), file) == NULL) {
+            fprintf(stderr, "%s: get_random_line(): error: %s\n", program_name, strerror(errno));
+            exit(errno);
+        }
+    }
+
+    // read and process the chosen line
+    if (fgets(line_bfr, sizeof(line_bfr), file) == NULL) {
+        fprintf(stderr, "%s: get_random_line(): error: %s\n", program_name, strerror(errno));
+        exit(errno);
+    }
+
+    // get the length of the string without the newline
+    size_t length = strcspn(line_bfr, "\n");
+
+    // Replace the newline character with null terminator
+    if (line[length] == '\n')
+        line[length] = '\0';
+
+    // trim the newline char and null terminate the buffer
+    line = line_bfr;
+
+    // Print the randomly chosen line
+    return line;
+}
+
+// perform some checks and initialize a file descriptor
+FILE *file_prepare(char *file_path, bool read_only, bool use_binary) {
+    // try to open the file for writing/creating
+    int fd = open(file_path, (read_only == true ? O_RDONLY : O_WRONLY | O_CREAT), S_IRUSR | S_IWUSR);
+
+    // print an error message and exit if the file can't be accessed
+    if (fd == -1) {
+        fprintf(stderr, "%s: cannot %s %s: %s\n", program_name, (read_only == true ? "read" : "create or read"), file_path, strerror(errno));
+        exit(errno);
+    }
+
+    // close the file descriptor if it was successfully opened
+    close(fd);
+
+    // open the file as a text or binary stream
+    if (use_binary)
+        return fopen(file_path, (read_only == true ? "rb" : (option_append_writes == true ? "a+b" : "w+b")));
+    else
+        return fopen(file_path, (read_only == true ? "r" : (option_append_writes == true ? "a+" : "w+")));
+}
+
 // generate a random ASCII number/uppercase character
 char get_random_char(bool is_ucase) {
     // get the random number
@@ -90,6 +194,10 @@ char get_random_char(bool is_ucase) {
 
 // pick a random name from the first_names[] array
 const char *get_random_name(void) {
+    // use the first_names[] array or a provided file in names_file
+    if (names_location)
+        return file_get_random_line(names_location);
+
     // store the amount of names in the array
     unsigned int name_ct = sizeof(first_names) / sizeof(first_names[0]);
 
@@ -101,7 +209,7 @@ const char *get_random_name(void) {
 }
 
 // print a hostname like DESKTOP-XXXXXXX: format_prefix is printed before, format_suffix is printed after
-void hostname_windows(FILE *hostname_fd, char *format_prefix, char *format_suffix) {
+void hostname_windows(char *format_prefix, char *format_suffix) {
     // char array containing the random part of the string
     char win_rand[8];
 
@@ -111,11 +219,11 @@ void hostname_windows(FILE *hostname_fd, char *format_prefix, char *format_suffi
     }
 
     // print the hostname
-    fprintf(hostname_fd, "%s%s%s%s\n", format_prefix, "DESKTOP-", win_rand, format_suffix);
+    fprintf(write_location, "%s%s%s%s\n", format_prefix, "DESKTOP-", win_rand, format_suffix);
 }
 
 // print a hostname like [Name]s-Macbook-[Air|Pro]
-void hostname_macbook(FILE *hostname_fd, char *format_prefix, char *format_suffix) {
+void hostname_macbook(char *format_prefix, char *format_suffix) {
     // type of macbook
     char mac_type[3];
 
@@ -123,31 +231,56 @@ void hostname_macbook(FILE *hostname_fd, char *format_prefix, char *format_suffi
     (rand() % 2) == 0 ? strncpy(mac_type, "Air", 3) : strncpy(mac_type, "Pro", 3);
 
     // print the hostname
-    fprintf(hostname_fd, "%s%ss-Macbook-%s%s\n", format_prefix, get_random_name(), mac_type, format_suffix);
+    fprintf(write_location, "%s%ss-Macbook-%s%s\n", format_prefix, get_random_name(), mac_type, format_suffix);
 }
 
 // print a hostname like [Name]s-Mac-Mini
-void hostname_macmini(FILE *hostname_fd, char *format_prefix, char *format_suffix) {
+void hostname_macmini(char *format_prefix, char *format_suffix) {
     // print the hostname
-    fprintf(hostname_fd, "%s%ss-Mac-Mini%s\n", format_prefix, get_random_name(), format_suffix);
+    fprintf(write_location, "%s%ss-Mac-Mini%s\n", format_prefix, get_random_name(), format_suffix);
 }
 
-// perform some checks and initialize a file descriptor
-FILE *prepare_file(char *file_path) {
-    // try to open the file for writing/creating
-    int fd = open(file_path, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+// set platform_type enum
+enum EPlatformType set_platform_type(char *type_name) {
+    // spaghetti ☠️  (doesn't matter it's not in main() anymore)
+    if (strncmp(type_name, "mac", 4) == 0)
+        return (rand() % 2) == 0 ? MACBOOK : MACMINI;
+    if (strncmp(type_name, "macos", 6) == 0)
+        return (rand() % 2) == 0 ? MACBOOK : MACMINI;
+    if (strncmp(type_name, "osx", 4) == 0)
+        return (rand() % 2) == 0 ? MACBOOK : MACMINI;
+    if (strncmp(type_name, "macbook", 4) == 0)
+        return MACBOOK;
+    if (strncmp(type_name, "macmini", 4) == 0)
+        return MACMINI;
+    if (strncmp(type_name, "windows", 8) == 0)
+        return WINDOWS;
+    if (strncmp(type_name, "win7", 5) == 0)
+        return WINDOWS;
+    if (strncmp(type_name, "win8", 5) == 0)
+        return WINDOWS;
+    if (strncmp(type_name, "win10", 6) == 0)
+        return WINDOWS;
+    if (strncmp(type_name, "win11", 6) == 0)
+        return WINDOWS;
 
-    // print an error message and exit if the file can't be accessed
-    if (fd == -1) {
-        fprintf(stderr, "%s: cannot read or create %s: %s\n", program_name, file_path, strerror(errno));
-        exit(errno);
-    }
+    // if the platform type is to be chosen by chance, do so
+    int platform_rng = rand() % 12;
 
-    // close the file descriptor if it was successfully opened
-    close(fd);
+    // 33% chance to be macbook
+    if (platform_rng >= 0)
+        return MACBOOK;
 
-    // return the file descriptor
-    return fopen(file_path, (option_append_writes == true ? "a+" : "w+"));
+    // 16% chance to be mac mini
+    if (platform_rng >= 4)
+        return MACMINI;
+
+    // 50% chance to be windows
+    if (platform_rng >= 6)
+        return WINDOWS;
+
+    // fallback (this shouldn't happen)
+    return ANY;
 }
 
 // main
@@ -159,16 +292,12 @@ int main(int argc, char *argv[]) {
     program_name = argv[0];
 
     // open /dev/urandom for reading
-    int random_file = open("/dev/urandom", O_RDONLY);
+    random_file = file_prepare("/dev/urandom", true, true);
 
-    // print an error message and exit if the file can't be accessed
-    if (random_file == -1) {
-        fprintf(stderr, "%s: cannot read or create %s: %s\n", program_name, "/dev/urandom", strerror(errno));
-        exit(errno);
-    }
+    // read bytes from /dev/urandom
+    read(fileno(random_file), &random_seed, sizeof(random_seed));
 
-    // seed the random number generator with entropy from /dev/urandom
-    read(random_file, &random_seed, sizeof(random_seed));
+    // seed the random number generator with entropy
     srand(random_seed);
 
     // current option, index
@@ -178,7 +307,7 @@ int main(int argc, char *argv[]) {
     enum EPlatformType platform_type = ANY;
 
     // parse options
-    while ((opt = getopt_long(argc, argv, "aho:p:P:s:v", long_options, &option_index)) != -1) {
+    while ((opt = getopt_long(argc, argv, "ahn:o:p:P:s:v", long_options, &option_index)) != -1) {
         switch (opt) {
             case 'a':
                 option_append_writes = true;
@@ -189,28 +318,11 @@ int main(int argc, char *argv[]) {
             case 'o':
                 option_output_file = strdup(optarg);
                 break;
+            case 'n':
+                option_names_file = strdup(optarg);
+                break;
             case 'p':
-                // spaghetti ☠️
-                if (strncmp(optarg, "mac", 4) == 0)
-                    platform_type = (rand() % 2) == 0 ? MACBOOK : MACMINI;
-                if (strncmp(optarg, "macos", 6) == 0)
-                    platform_type = (rand() % 2) == 0 ? MACBOOK : MACMINI;
-                if (strncmp(optarg, "osx", 4) == 0)
-                    platform_type = (rand() % 2) == 0 ? MACBOOK : MACMINI;
-                if (strncmp(optarg, "macbook", 4) == 0)
-                    platform_type = MACBOOK;
-                if (strncmp(optarg, "macmini", 4) == 0)
-                    platform_type = MACMINI;
-                if (strncmp(optarg, "windows", 8) == 0)
-                    platform_type = WINDOWS;
-                if (strncmp(optarg, "win7", 5) == 0)
-                    platform_type = WINDOWS;
-                if (strncmp(optarg, "win8", 5) == 0)
-                    platform_type = WINDOWS;
-                if (strncmp(optarg, "win10", 6) == 0)
-                    platform_type = WINDOWS;
-                if (strncmp(optarg, "win11", 6) == 0)
-                    platform_type = WINDOWS;
+                option_platform = strdup(optarg);
                 break;
             case 'P':
                 option_prefix = strdup(optarg);
@@ -224,37 +336,24 @@ int main(int argc, char *argv[]) {
                 display_version_message();
                 break;
             default:
-                // Handle other cases
                 break;
         } // yeah I used strdup() without freeing memory, sue me 🙂
     }
 
+    // open the name file if one is provided
+    names_location = (option_names_file) ? file_prepare(option_names_file, true, false) : NULL;
+
     // open the chosen file or point to stdout
-    FILE *write_location = (option_output_file) ? prepare_file(option_output_file) : stdout;
+    write_location = (option_output_file) ? file_prepare(option_output_file, false, false) : stdout;
 
-    // if the platform type is to be chosen by chance, do so
-    if (platform_type == ANY) {
-        // get a number
-        int platform_rng = rand() % 12;
-
-        // 33% chance to be macbook
-        if (platform_rng >= 0)
-            platform_type = MACBOOK;
-
-        // 16% chance to be mac mini
-        if (platform_rng >= 4)
-            platform_type = MACMINI;
-
-        // 50% chance to be windows
-        if (platform_rng >= 6)
-            platform_type = WINDOWS;
-    }
+    // set or randomize platform_type
+    platform_type = set_platform_type(option_platform);
 
     // run the specified hostname function
     switch (platform_type) {
-        case MACBOOK: hostname_macbook(write_location, option_prefix, option_suffix); break;
-        case MACMINI: hostname_macmini(write_location, option_prefix, option_suffix); break;
-        case WINDOWS: hostname_windows(write_location, option_prefix, option_suffix); break;
+        case MACBOOK: hostname_macbook(option_prefix, option_suffix); break;
+        case MACMINI: hostname_macmini(option_prefix, option_suffix); break;
+        case WINDOWS: hostname_windows(option_prefix, option_suffix); break;
         default: break;
     }
 
